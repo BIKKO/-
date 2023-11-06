d62043c1aa1b1030092a3ff15462eb755f987d77
BIKKO <njnjirf238@gmail.com>
2023-11-06 23:27:41 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_для_решения
+{
+    class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Score { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
